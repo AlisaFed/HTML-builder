79e79c299f6fd4fc929b396dae9ec2b121c23569
alisafed <alisa.fed.91@gmail.com>
2022-05-24 01:09:28 +0300

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,53 @@
+const fs = require ('fs');
+const path = require ('path');
 
+fs.mkdir('./04-copy-directory/files-copy', err => {
+    if(err) {fs.rmdir('./04-copy-directory/files-copy', err => {
+    if(err) {
+        fs.readdir ('./04-copy-directory/files-copy', {withFileTypes: true}, (error, dirEntryList) => {
+            if(!error) {
+                dirEntryList.forEach((dirEntry) => {
+                    if (dirEntry.isFile()) {
+                        fs.unlink(`./04-copy-directory/files-copy/${dirEntry.name}`, err => {
+                            if(err) throw err;
+                         });
+                    } fs.rmdir('./04-copy-directory/files-copy', err => {
+                        if(!err) {
+                            fs.mkdir('./04-copy-directory/files-copy', err => {})
+                            fs.readdir ('./04-copy-directory/files', {withFileTypes: true}, (error, dirEntryList) => {
+                                if(!error) {
+                                    dirEntryList.forEach((dirEntry) => {
+                                        if (dirEntry.isFile()) {
+                                            fs.copyFile(`./04-copy-directory/files/${dirEntry.name}`, `./04-copy-directory/files-copy/${dirEntry.name}`, error => {
+                                                if(error) throw error; 
+                                             });
+                                        }
+                                    });
+                                } else {
+                                    console.error(error);
+                                };
+                            });
+                        };})
+                });
+            } else {
+                console.error(error);
+            };
+        });
+    }
+})}
+})
+
+
+fs.readdir ('./04-copy-directory/files', {withFileTypes: true}, (error, dirEntryList) => {
+    if(!error) {
+        dirEntryList.forEach((dirEntry) => {
+            if (dirEntry.isFile()) {
+                fs.copyFile(`./04-copy-directory/files/${dirEntry.name}`, `./04-copy-directory/files-copy/${dirEntry.name}`, error => {
+                    if(error) throw error; 
+                 });
+            }
+        });
+    } else {
+        console.error(error);
+    };
+});
