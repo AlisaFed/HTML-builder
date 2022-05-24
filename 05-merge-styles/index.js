30f8f05879e6c5b0c7b16a534dd5e6f842d70b54
alisafed <alisa.fed.91@gmail.com>
2022-05-24 20:06:03 +0300

    feat: add fifth task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const fs = require ('fs');
+const path = require ('path');
+const ws = fs.createWriteStream('./05-merge-styles/project-dist/bundle.css');
+const Stream = require('stream');
+const writableStream = new Stream.Writable();
+
+fs.readdir ('./05-merge-styles/styles', {withFileTypes: true}, (error, dirEntryList) => {
+    if(!error) {
+        dirEntryList.forEach((dirEntry) => {
+            if (dirEntry.isFile() && `${path.extname(dirEntry.name)}` == '.css') {
+                console.log(`${dirEntry.name}`, `${path.extname(dirEntry.name)}`);
+                let readableStream = fs.createReadStream(`./05-merge-styles/styles/${dirEntry.name}`, 'utf-8');
+                readableStream.on("data", function(chunk){ 
+                    fs.appendFile('./05-merge-styles/project-dist/bundle.css', chunk, function(error){
+                        if(error) throw error; 
+                        console.log('Данные успешно записаны записать файл');
+                     });
+                });
+            }
+        });
+    } else {
+        console.error(error);
+    };
+});
+
